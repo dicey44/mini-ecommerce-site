9e4190027f99a6dbb9301918a770b2d66c0246a9
dicey44 <226594125+dicey44@users.noreply.github.com>
2026-08-09 18:52:29 -0400

    chore: add quantity to be visible

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -12,9 +12,10 @@ export default function Cart( { cart, removeFromCart }: CartAppProps) {
     return (
         <div>
             <h1>My Cart</h1>
-            {cart.map(({ product }: CartProps, index: number) => {
+            {cart.map(({ product, quantity }: CartProps, index: number) => {
                 return (
                     <div key={index}>{product.name}<span>{" $" + product.price + " "}</span>
+                        <p>Quantity: {quantity}</p>
                         <button onClick={() => removeFromCart(product)}>Remove From Cart</button>
                     </div>
                 );
