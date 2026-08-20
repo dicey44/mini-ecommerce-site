bbea1dfb5002b81f2c1e7adaa4278068c80d8f1d
dicey44 <226594125+dicey44@users.noreply.github.com>
2026-08-20 11:26:55 -0400

    style: style cart

--- src/Cart.tsx
+++ src/Cart.tsx
@@ -1,6 +1,7 @@
 import CartItemComp from "./CartItemComp";
 import type { CartItem } from "./types/product";
 import { useCart } from "./CartContext";
+import "./Cart.css"
 
 
 
@@ -8,19 +9,37 @@ export default function Cart() {
     
     const { cart } = useCart();
 
-    return (
-        <div>
-            <h1>My Cart</h1>
-            {cart.map(( item: CartItem) => {
-                return (
-                    <CartItemComp cartListing={item} key={item.product.id}/>
-                        
-                );
-            })}
-
-            <h3>Total: ${cart.reduce((accumulator, currentItem) => {
+    const subTotal = cart.reduce((accumulator, currentItem) => {
                 return (currentItem.product.price * currentItem.quantity) + accumulator;
-            }, 0)}</h3>
+            }, 0);
+
+    const totalTax = subTotal * 0.065;
+
+    return (
+        <div className={(cart.length === 0 ? "hide" : "cart")}>
+            <h2>My Cart</h2>
+            <h3>Items</h3>
+            <div className="cart-items">
+                {cart.map(( item: CartItem) => {
+                    return (
+                        <CartItemComp cartListing={item} key={item.product.id}/>
+                            
+                    );
+                })}
+            </div>
+            
+            <div className="totals">
+                <span >Subtotal</span><span>${subTotal}</span>
+            </div>
+            <div className="totals">
+                <span className="total-tax">Tax</span><span>${totalTax.toFixed(2)}</span>
+            </div>
+            <div className="totals total-amount">
+                <span>Total</span><span>${(subTotal + totalTax).toFixed(2)}</span>
+            </div>
+            <button className="proceed-to-checkout-btn">Proceed to Checkout</button>
+            
+            
         </div>
     )
 }
--- src/CartItemComp.tsx
+++ src/CartItemComp.tsx
@@ -10,15 +10,17 @@ export default function CartItemComp( { cartListing }: CartItemProps ) {
     const { removeFromCart, addToCart } = useCart();
 
     return (
-         <div>
-            <img src={cartListing.product.image_url} width="150"/>
-            <p>{cartListing.product.name}<span>{" $" + cartListing.product.price + " "} x {cartListing.quantity}</span></p>
-
-                        <strong>${cartListing.quantity * cartListing.product.price}</strong>
-                        <br/>
+         <div className="cart-item">
+            
+            <p>{cartListing.product.name}<span> x {cartListing.quantity}</span></p>
+                
+            <strong>${cartListing.quantity * cartListing.product.price}</strong>
+            <br/>
+            <div className="cart-item-buttons">
+                <button onClick={() => removeFromCart(cartListing.product)}>Remove -</button>
+                <button onClick={() => addToCart(cartListing.product)}>Add +</button>
+            </div>
                         
-                        <button onClick={() => removeFromCart(cartListing.product)}>Remove 1</button>
-                        <button onClick={() => addToCart(cartListing.product)}>Add 1</button>
         </div>
     )
 }
