869fc42f84023a43befd490ae895c35b47a0dcfd
dicey44 <226594125+dicey44@users.noreply.github.com>
2026-09-01 13:50:11 -0400

    fix: change items in cart to show yen sign instead of dollar sign

--- src/CartItemComp.tsx
+++ src/CartItemComp.tsx
@@ -15,13 +15,14 @@ export default function CartItemComp( { cartListing }: CartItemProps ) {
 
     const productName = language === "en" ? cartListing.product.name : productTranslations[cartListing.product.id].name;
     const productTotalPrice = language === "en" ? (cartListing.quantity * cartListing.product.price).toFixed(2) : productTranslations[cartListing.product.id].price * cartListing.quantity;
+    const moneySign = language === "en" ? "$" : "¥"
 
     return (
          <div className="cart-item">
             
             <p>{productName}<span> x {cartListing.quantity}</span></p>
                 
-            <strong>${productTotalPrice}</strong>
+            <strong>{moneySign}{productTotalPrice}</strong>
             <br/>
             <div className="cart-item-buttons">
                 <button onClick={() => removeFromCart(cartListing.product)}>Remove -</button>
